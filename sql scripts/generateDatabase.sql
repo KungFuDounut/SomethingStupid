b3873491a35440455d1132185db79e4727540bf9
Tyron <tyronduvenage@hotmail.com>
2019-11-16 00:04:19 +0200

    Database generation script

---
+++ sql scripts/generateDatabase.sql
@@ -0,0 +1,26 @@
+--DROP THE DATABASE IF IT EXISTS
+DECLARE @dbname nvarchar(50)
+SET @dbname = N'SomethingStupid'
+IF (EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE ('[' + name + ']' = @dbname OR name = @dbname)))
+BEGIN
+    DROP DATABASE SomethingStupid
+END
+--CREATE A NEW DATABASE
+CREATE DATABASE SomethingStupid;
+USING SomethingStupid
+--CREATE THE ONLY TABLE NEEDED
+CREATE TABLE orgUser(
+    UserId INT IDENTITY(1,1) NOT NULL
+    ,Username VARCHAR(50) NOT NULL
+    ,Password VARCHAR(50) NOT NULL
+    ,LoginCounter INT DEFAULT 0
+    ,CreatedDate DATETIME DEFAULT GETDATE()
+);   
+--SEEDED DATA FOR TESTING
+INSERT INTO orgUser
+(Username, Password, LoginCounter)
+VALUES
+('Username1','Password1',0),
+('Username2','Password2',1),
+('Username3','Password3',2),
+('Username4','Password4',3)
